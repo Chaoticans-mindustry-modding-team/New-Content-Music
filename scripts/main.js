802c23da8975ab5bb4b10dd9d77ca399b8b2d5d2
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2025-11-21 07:39:23 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,16 +13,43 @@ let game10 = Vars.tree.loadMusic("game10"); //fanmade
 let boss3 = Vars.tree.loadMusic("boss3"); //fanmade boss
 let fine2 = Vars.tree.loadMusic("fine2"); //ye
 let cosmos = Vars.tree.loadMusic("cosmos"); //ye
-
-Events.on(ClientLoadEvent, e => {
-  Vars.control.sound.ambientMusic.add(day2);
-  Vars.control.sound.ambientMusic.add(fine2);
-  Vars.control.sound.ambientMusic.add(cosmos);
-  Vars.control.sound.darkMusic.add(day3);
-  Vars.control.sound.darkMusic.add(day4);
-  Vars.control.sound.darkMusic.add(game10);
-  Vars.control.sound.darkMusic.add(cosmos);
-  Vars.control.sound.bossMusic.add(boss3);
-  Vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
-  Vars.control.sound.darkMusic.add(seq);
+let planet = null, planetCur = null;
+function clearMusic() {
+  Vars.control.sound.ambientMusic = Seq.with();
+  Vars.control.sound.darkMusic = Seq.with();
+  Vars.control.sound.bossMusic = Seq.with();
+}
+function callOnPlanetChange(planet) {
+  switch(planet)
+    case (Planets.serpulo):
+    case (Planets.erekir):
+        Vars.control.sound.reload();
+        Vars.control.sound.ambientMusic.add(day2);
+        Vars.control.sound.ambientMusic.add(fine2);
+        Vars.control.sound.ambientMusic.add(cosmos);
+        Vars.control.sound.darkMusic.add(day3);
+        Vars.control.sound.darkMusic.add(day4);
+        Vars.control.sound.darkMusic.add(game10);
+        Vars.control.sound.darkMusic.add(cosmos);
+        Vars.control.sound.bossMusic.add(boss3);
+        Vars.control.sound.bossMusic.add(seq); //duck it, seq is boss music
+        Vars.control.sound.darkMusic.add(seq);
+        return;
+      case(Vars.content.planet(newunits-clexon)):
+        clearMusic();
+        Vars.control.sound.ambientMusic.add(cosmos);
+        Vars.control.sound.ambientMusic.add(day3);
+        Vars.control.sound.ambientMusic.add(day4);
+        Vars.control.sound.darkMusic.add(cosmos);
+        Vars.control.sound.darkMusic.add(game10);
+        Vars.control.sound.darkMusic.add(seq);
+        Vars.control.sound.bossMusic.add(seq);
+        return;
+};
+let planet = null, planetCur = null;
+Events.on(Trigger.update, () => {
+  if(Vars.state.isMenu() || Vars.state.isEditor()) return;
+  planetCur = Vars.state.getPlanet();
+  if(planetCur != planet) callOnPlanetChange(planetCur);
+  planet = planetCur;
 });
